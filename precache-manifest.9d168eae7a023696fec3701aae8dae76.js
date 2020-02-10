6e0307d16e1049db1b48d3727818d8c169ec407a
SagarGi <sagargurung1001@gmail.com>
2020-02-10 19:50:06 +0545

    Updates

---
+++ precache-manifest.9d168eae7a023696fec3701aae8dae76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715aecaf6e062b8311662ce12694f00f",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "db1ec3378c26e924a1ff",
+    "url": "/MyPortfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "dbcb761a127ce6dddcba",
+    "url": "/MyPortfolio/static/css/main.82054b50.chunk.css"
+  },
+  {
+    "revision": "db1ec3378c26e924a1ff",
+    "url": "/MyPortfolio/static/js/2.5078e209.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/MyPortfolio/static/js/2.5078e209.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbcb761a127ce6dddcba",
+    "url": "/MyPortfolio/static/js/main.b52bdd74.chunk.js"
+  },
+  {
+    "revision": "c38943c7024563f6bf93",
+    "url": "/MyPortfolio/static/js/runtime-main.ab99c11d.js"
+  },
+  {
+    "revision": "8885ce9860f3aed4c999dc1b9fb70633",
+    "url": "/MyPortfolio/static/media/java.8885ce98.png"
+  },
+  {
+    "revision": "18f6e1631d88619cf2fb0c0c649f6316",
+    "url": "/MyPortfolio/static/media/sagar.18f6e163.jpg"
+  }
+]);
