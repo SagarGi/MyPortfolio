afb3e10f2cddfa9940ec809b5a5c59dd74c04457
SagarGi <sagargurung1001@gmail.com>
2020-02-22 18:42:16 +0545

    qr update

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import MyPicture from './MyPicture';
 import './portfolio.css'
 import ButtonBar from './ButtonBar';
@@ -6,39 +6,35 @@ import Follow from './Follow';
 
 
 
-class Portfolio extends Component
-{
-    render()
-    {
-        return(
-        <div>
-            <div className = "container-fluid">
-                {/* welcome to my world section */}
-                <section>
-                <div className = "text-white">
-                    <h1 className = "text text-center text-capitalize mt-5 display-4 font-weight-bold">Welcome To My World!!</h1>
+class Portfolio extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container-fluid">
+
+                    <div>
+
+                        <h1 className="text text-center text-capitalize mt-5 display-4 font-weight-bold">Welcome To My World!! <img className="qr" src={require("./images/sagarqrcode.png")} height="50" width="50" /> </h1>
+                    </div>
                 </div>
-                {/* wlcome to my world ends */}
-                </section>
-            </div>
-            <div className = "Container-fluid">
+                <div className="Container-fluid">
+                    <section>
+                        <MyPicture />
+                        <p className="textpara text-center  h3 font-weight-bold">Hello world, I am Sagar Gurung.</p>
+                        <p className="textpara text-center  h3 font-weight-bold">Java makes me cum hard but i get horny when i hear c, c++ and javascript.</p><br></br>
+                        <hr className="hrline"></hr>
+                    </section>
+                </div>
+
                 <section>
-                    <MyPicture />
-                    <p className = "textpara text-center  h3 font-weight-bold">Hello world, I am Sagar Gurung.</p>
-                    <p className = "textpara text-center  h3 font-weight-bold">Java makes me cum hard but i get horny when i hear c, c++ and javascript.</p><br></br>
-                    <hr className = "hrline"></hr>
+                    <ButtonBar />
+                </section>
+
+                <section>
+                    <Follow />
                 </section>
             </div>
 
-            <section>
-                <ButtonBar />
-            </section>
-
-            <section>
-                <Follow />
-            </section>
-        </div>
-           
         )
     }
 }
