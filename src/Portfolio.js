ab1a0323ff225206946f442f546e26feda07760a
SagarGi <sagargurung1001@gmail.com>
2020-04-01 19:29:12 +0545

    pp

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,12 +1,19 @@
 import React, {component, Component} from 'react'
+import './portfolio.css'
+import Avatar from 'react-avatar'
 
 class Portfolio extends Component
 {
     render()
     {
         return(
-            <div>
-                <p>This is  going to be my portfolio.</p>
+            <div className = "main">
+                <div className = "about-me container text-center">
+                        <div className = "profile-image text-center">
+                            <img src = {require('./images/sagar.jpg')} alt = "SagarGi" className = "avatar"></img>
+                        </div>
+                        <h1>About Me</h1>
+                </div>
             </div>
         )
     }
