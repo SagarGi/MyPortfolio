bf2c862850b07ec5833e88605c8c6bd5dd10a3df
SagarGi <sagargurung1001@gmail.com>
2020-04-01 18:31:30 +0545

    revive

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,40 +1,13 @@
-import React, { Component } from 'react'
-import MyPicture from './MyPicture';
-import './portfolio.css'
-import ButtonBar from './ButtonBar';
-import Follow from './Follow';
+import React, {component, Component} from 'react'
 
-
-
-class Portfolio extends Component {
-    render() {
-        return (
+class Portfolio extends Component
+{
+    render()
+    {
+        return(
             <div>
-                <div className="container-fluid">
-
-                    <div>
-
-                        <h1 className="text text-center text-capitalize mt-5 display-4 font-weight-bold">Welcome To My World!! <img className="qr" src={require("./images/sagarqrcode.png")} height="50" width="50" /> </h1>
-                    </div>
-                </div>
-                <div className="Container-fluid">
-                    <section>
-                        <MyPicture />
-                        <p className="textpara text-center  h3 font-weight-bold">Hello world, I am Sagar Gurung.</p>
-                        <p className="textpara text-center  h3 font-weight-bold">Java makes me cum hard but i get horny when i hear c, c++ and javascript.</p><br></br>
-                        <hr className="hrline"></hr>
-                    </section>
-                </div>
-
-                <section>
-                    <ButtonBar />
-                </section>
-
-                <section>
-                    <Follow />
-                </section>
+                <p>This is  going to be my portfolio.</p>
             </div>
-
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Portfolio from './Portfolio';
 import * as serviceWorker from './serviceWorker';
