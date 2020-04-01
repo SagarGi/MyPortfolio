a9c3a339dd0b70d215d1f2c24d25cd59aac97420
SagarGi <sagargurung1001@gmail.com>
2020-04-01 20:41:10 +0545

    about me added

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -13,6 +13,17 @@ class Portfolio extends Component
                             <img src = {require('./images/sagar.jpg')} alt = "SagarGi" className = "avatar"></img>
                         </div>
                         <h1>About Me</h1>
+                        <p>Hello World,<i class="fa fa-hand-paper-o wave" aria-hidden="true"></i> My name is Sagar Gurung.<br></br>I am a software engineering student persuing BE in software in GCES.
+                        <br></br>I like java most than other programing language but i also like c,c++ and javascript.<br></br>
+                        I am a learning developer, you can find some of my projects below. Beside that i also write <a href = "https://www.saagargurung.com.np/" target = "_blank">BLOGS</a></p>
+                </div>
+
+                <div className = "container">
+
+                    <div className = "proejcts text-center mt-4">
+                            <h1>My Projects</h1>
+                    </div>
+
                 </div>
             </div>
         )
