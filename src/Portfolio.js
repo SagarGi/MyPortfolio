eb7e4e9ed5f0c7aa53b9d5b16a03937a17f68a2e
SagarGi <sagargurung1001@gmail.com>
2020-04-02 09:31:49 +0545

    godown

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -4,13 +4,26 @@ import Avatar from 'react-avatar'
 
 class Portfolio extends Component
 {
+    constructor()
+    {
+        super()
+    }
+
+    goDown = () =>
+    {
+        // alert("hello i am clicked")
+        var element_id = document.getElementById('project')
+        element_id.scrollIntoView()
+    }
+
+   
     render()
     {
         return(
             <div className = "main">
                 <div className = "about-me container text-center">
                         <div className = "profile-image text-center">
-                            <img src = {require('./images/sagar.jpg')} alt = "SagarGi" className = "avatar"></img>
+                            <img src = {require('./images/sagar.jpg')} alt = "SagarGi" class = "avatar"></img>
                         </div>
                         <h1>About Me</h1>
                         <p>Hello World,<i class="fa fa-hand-paper-o wave" aria-hidden="true"></i> My name is Sagar Gurung.<br></br>I am a software engineering student pursuing BE in software in GCES.
@@ -20,8 +33,20 @@ class Portfolio extends Component
 
                 <div className = "container">
 
-                    <div className = "projects text-center mt-5">
+                    <div className = "projects text-center" id = "project">
                             <h1>My Projects</h1>
+                            <div>
+                            <i class="fa fa-angle-double-down icon-down" onClick = {this.goDown}></i>
+                            </div>   
+                    </div>
+
+                </div>
+
+                {/* projects section  */}
+                <div className = "container">
+
+                    <div className = "myprojects">
+                        <p>Some projects</p>
                     </div>
 
                 </div>
